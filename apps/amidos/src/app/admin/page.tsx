5a8984b784c317d0e999d85d2f9fb2cb697d17e4
munkhuzl <bolzop.zulaa@gmail.com>
2024-10-18 18:25:34 +0800

    amidos: add shadcn components (#71)
    Co-authored-by: Odonkhuu Munkhzul <odonkhuumunkhzul@Odonkhuus-MacBook-Pro.local>

--- apps/amidos/src/app/admin/page.tsx
+++ apps/amidos/src/app/admin/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from '../components/ui/button';
-
 export default function Home() {
   return (
     <div>
-      <div>
-        <Button>Admin</Button>
-      </div>
+      <div></div>
     </div>
   );
 }
