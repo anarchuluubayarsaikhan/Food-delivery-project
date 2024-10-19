e4a1f37d6aa4597de9bc7a4f7180f4be2ec34fa5
badral <badralbaigalmaa7@gmail.com>
2024-10-19 13:59:21 +0800

    superduper: shadcn setup done (#69)
    Co-authored-by: badral <badral@badrals-MacBook-Pro.local>

--- apps/superduper/src/app/client/productDetails/page.tsx
+++ apps/superduper/src/app/client/productDetails/page.tsx
@@ -4,6 +4,7 @@ import { Bid } from '../../components/Bid';
 import { HelpCenter } from '../../components/helpCenter';
 import { ProductDetailImages } from '../../components/ProductDetailImages';
 import { Safity } from '../../components/Safity';
+import { Button } from '../../components/ui/Button';
 export default function App() {
   return (
     <div className="max-w-[1240px] mx-auto w-full">
@@ -13,6 +14,7 @@ export default function App() {
           <Bid />
           <Safity />
           <HelpCenter />
+          <Button>badral</Button>
         </div>
       </div>
     </div>
--- apps/superduper/src/app/components/Bid.tsx
+++ apps/superduper/src/app/components/Bid.tsx
@@ -1,6 +1,28 @@
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+import { Button } from './ui/Button';
+import { Input } from './ui/Input';
+
+interface FormValues {
+  bid: string;
+}
 export const Bid = () => {
+  const validationSchema = yup.object({
+    bid: yup.number().min(1000, 'too low price').required('une oruul'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      bid: 0,
+    },
+    onSubmit: async (values, { resetForm }) => {
+      fetch('/api/');
+    },
+    validationSchema,
+  });
   return (
-    <div>
+    <form onSubmit={formik.handleSubmit}>
       <div>Closes in 4d 4h 04m 35s</div>
       <div className="border-l-2 border-b-2 border-slate-300">
         <div className="mt-3 border-t-2 border-blue-600 py-8 px-6">
@@ -14,11 +36,13 @@ export const Bid = () => {
           <div>3000</div>
           <label className="border-solid bg-[#f8f7f8] flex gap-1 items-center py-1 px-3 w-full">
             <div className="text-slate-500">€</div>
-            <input className="w-full p-2 bg-[#f8f7f8]" placeholder="3,350 or up" type="number" />
+            <Input id="bid" onChange={formik.handleChange} value={formik.values.bid !== 0 ? formik.values.bid : ''} className="w-full p-2 bg-[#f8f7f8]" placeholder="3,350 or up" type="number" />
           </label>
           <div className="flex gap-1 w-full">
-            <div className="flex-1 border-[1px] py-2 px-4 text-blue-500 text-center">Place bid</div>
-            <div className="flex-1 border-[1px] py-2 px-4 bg-blue-600 text-white text-center">Set max bid</div>
+            <Button className="flex-1 border-[1px] py-2 px-4 bg-white text-blue-500 text-center">Place bid</Button>
+            <Button type="submit" className="flex-1 border-[1px] py-2 px-4 bg-blue-600 text-white text-center">
+              Set max bid
+            </Button>
           </div>
         </div>
         <div className="mt-8 px-4">Buy confidently with our Buyer Protection</div>
@@ -36,6 +60,6 @@ export const Bid = () => {
           <div className="mb-2">See all bids (7)</div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
