62bf12e7dda15579c61b737828489ad68c2e7929
Delgermurun <88150265+Delgermurun17@users.noreply.github.com>
2024-10-28 15:00:13 +0800

    ger-gurus: otp generate (#207)

---
+++ apps/ger-gurus/src/app/api/otp/route.tsx
@@ -0,0 +1,37 @@
+import { db } from '@/lib/db';
+import nodemailer from 'nodemailer';
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { email } = body;
+  const otp = Math.floor(Math.random() * 8999) + 1000;
+  const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
+  try {
+    const createdAt = new Date();
+    await db.collection('otp').insertOne({
+      email,
+      otp,
+      createdAt,
+    });
+    await db.collection('otp').createIndex({ createdAt: 1 }, { expireAfterSeconds: 300 });
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: '88delgeree@gmail.com',
+        pass: GOOGLE_SECRET,
+      },
+    });
+
+    await transporter.sendMail({
+      from: '88delgeree@gmail.com',
+      to: email,
+      subject: 'OTP Verification',
+      html: `<p>Your OTP for verification is: <b>${otp}</b></p>`,
+    });
+    return new Response('OTP sent successfully', { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response('OTP send failed.', { status: 400 });
+  }
+}
