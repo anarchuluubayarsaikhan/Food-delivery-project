96e9d5ed1fe67effb1983c08d4b180a2bce34dd5
Tommy9901 <temuuujin0317@gmail.com>
2024-11-04 16:56:08 +0800

    259 amidos/admintable (#360)
    Co-authored-by: 1111 <a1111@1111s-MacBook-Pro.local>

--- apps/amidos/src/app/admin/table/page.tsx
+++ apps/amidos/src/app/admin/table/page.tsx
@@ -7,46 +7,37 @@ import LeftBar from '../components/leftbar';
 
 export type TableModel = {
   id: string;
-  corditane: {
+  coorditane: {
     x: number;
     y: number;
   };
 };
 
-export default function DragExample() {
-  const [tables, setTables] = useState(null);
-  const [addtable, setAddTable] = useState();
+export default function Table() {
+  const [tables, setTables] = useState([]);
 
   useEffect(() => {
     // fetch setTables();
   }, []);
 
-  function handleDrag(id: string, newPosition: { x: number; y: number }) {
-    const index = tables.findIndex((table) => table._id === id);
+  function handleDrag(index: number, newPosition: { x: number; y: number }) {
     const newTables = [...tables];
-    newTables[index].position = { x: newPosition.x, y: newPosition.y };
+    newTables[index].coordinate = { x: newPosition.x, y: newPosition.y };
     setTables(newTables);
   }
 
-  async function handleStop(_id: string, position: { x: number; y: number }) {
-    const updateTable = tables.find((table) => table._id === _id);
-    // fetch(`api/admin/tablesDetail`);
-    // TODO //PUT
+  async function handleStop(index: number) {
+    const current = tables[index];
 
-    setTimeout(async () => {
-      await fetch(`/api/admin/tablesDetail/${_id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-        body: JSON.stringify({
-          cordinate: updateTable?.position,
-        }),
-      });
-      getTables();
-    }, 1000);
-
-    console.log(updateTable);
+    await fetch(`/api/admin/tablesDetail/${current._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        coordinate: current?.coordinate,
+      }),
+    });
   }
 
   const getTables = async () => {
@@ -70,8 +61,7 @@ export default function DragExample() {
         'Content-type': 'application/json; charset=UTF-8',
       },
     });
-
-    setTables((prev) => [...prev, { _id: 'new', position: { x: 1, y: 1 } }]);
+    getTables();
   };
 
   const [selectedId, setSelectedId] = useState();
@@ -81,7 +71,6 @@ export default function DragExample() {
     await fetch(`/api/admin/tablesDetail${_id}`, {
       method: 'DELETE',
     });
-    getTables();
   };
 
   return (
@@ -90,9 +79,9 @@ export default function DragExample() {
       <div className="flex gap-10 mt-5">
         <div className="w-[800px] h-[800px] bg-slate-400 relative">
           {tables &&
-            tables.map((table: TableModel) => (
-              <Draggable key={table._id} position={table.cordinate} onDrag={(e, newPosition) => handleDrag(table._id, newPosition)} onStop={() => handleStop(table._id)}>
-                <div onClick={selectedId} className={`${table ? 'bg-green-400' : ''} absolute w-20 h-20`}></div>
+            tables.map((table: TableModel, index: number) => (
+              <Draggable key={table._id} position={table.coordinate} onDrag={(e, newPosition) => handleDrag(index, newPosition)} onStop={() => handleStop(index)}>
+                <div className={`${table ? 'bg-green-400' : ''} absolute w-20 h-20`}></div>
               </Draggable>
             ))}
         </div>
