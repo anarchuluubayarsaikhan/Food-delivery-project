a3494342133bf9e4f649e738a88f2fc3e6478074
UG <110281844+ugankhuyag@users.noreply.github.com>
2024-11-07 17:35:07 +0800

    superduper: tested photo add and mongodb (#435)
    Co-authored-by: 24LP5128 24LP5128 <24LP5128@Users-iMac.local>

--- apps/superduper/src/app/api/categories/route.ts
+++ apps/superduper/src/app/api/categories/route.ts
@@ -10,19 +10,19 @@ export async function POST(request: Request) {
     return new Response(null, { status: 201 });
   } catch (err) {
     return new Response(null, { status: 404 });
-
+  }}
 const collection = DB.collection('categories');
 
-export async function POST(request: Request) {
-  try {
-    const category = await request.json();
-    console.log(category)
-    const result = await collection.insertOne({ category });
-    return new Response(null, { status: 201 })
-  } catch (err) {
-    return new Response(null, { status: 404 })
-  }
-}
+// export async function POST(request: Request) {
+//   try {
+//     const category = await request.json();
+//     console.log(category)
+//     const result = await collection.insertOne({ category });
+//     return new Response(null, { status: 201 })
+//   } catch (err) {
+//     return new Response(null, { status: 404 })
+//   }
+// }
 export async function GET(request: Request) {
   try {
     const categories = await collection.find({}).toArray()
