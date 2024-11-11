3e0c895be56d3b6bfd85599fc90cfd574c65e2d6
UG <110281844+ugankhuyag@users.noreply.github.com>
2024-11-11 15:20:05 +0800

    482 superduper category client (#488)
    Co-authored-by: satsuralala <satsuraltumurbat@gmail.com>

--- apps/superduper/src/app/api/categories/route.ts
+++ apps/superduper/src/app/api/categories/route.ts
@@ -1,5 +1,8 @@
 import { DB } from '@/lib/db';
+import express from 'express';
 
+const app = express();
+let db;
 
 export async function POST(request: Request) {
   try {
@@ -23,12 +26,61 @@ const collection = DB.collection('categories');
 //     return new Response(null, { status: 404 })
 //   }
 // }
+// export async function GET(request: Request) {
+//   try {
+//     const categories = await collection.find({}).toArray()
+//     return Response.json(categories)
+//   } catch (err) {
+//     return Response.json({ message: "categories deer aldaa garlaa" })
+
+//   }
+// }
+
+// app.get('/categories', (req, res) => {
+//   let category: never[] = []
+
+//   DB.collection('categories')
+//     .find()
+//     .sort({category: 1})
+//     .forEach(categories => categories.push(categories))
+//     .then(() => {
+//       res.status(200).json(category)
+//     })
+//     .catch(() => {
+//       res.status(500).json({error: 'could not fetch the categories'})
+//     })
+//   })
+// function express() {
+//   throw new Error('Function not implemented.');
+// }
+
 export async function GET(request: Request) {
   try {
-    const categories = await collection.find({}).toArray()
-    return Response.json(categories)
-  } catch (err) {
-    return Response.json({ message: "categories deer aldaa garlaa" })
+    const collection = DB.collection('categories');
 
+    const result = await collection
+      .aggregate([
+        {
+          $lookup: {
+            from: 'categories',
+            localField: '_id',
+            foreignField: 'category',
+            as: 'categoryInfo',
+          },
+        },
+        {
+          $lookup: {
+            from: 'product',
+            localField: 'productId',
+            foreignField: 'category',
+            as: 'productInfo',
+          },
+        },
+        { $sort: { createdAt: -1 } },
+      ])
+      .toArray();
+    return Response.json(result);
+  } catch (err) {
+    return Response.json({ message: err });
   }
 }
--- apps/superduper/src/app/client/category/page.tsx
+++ apps/superduper/src/app/client/category/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
 
 type category = {
@@ -25,26 +26,18 @@ export default function Page() {
 
   return (
     <div>
-      <div>Ангилалууд</div>
-    <div className="container mx-auto flex gap-8">
-
-      <div>
-        <div className="bg-sky-100 h-40 w-96 rounded-sm items-center">Электроник</div>
-        <div className="bg-sky-200 h-40 w-96 rounded-sm">Урлаг ба цуглуулга</div>
-        <div className="bg-sky-300 h-40 w-96 rounded-sm">Загвар ба дагалдах хэрэгсэл</div>
-      </div>
-      <div>
-        <div className="bg-sky-400 h-40 w-96 rounded-sm">Үнэт эдлэл ба цаг</div>
-        <div className="bg-sky-500 h-40 w-96 rounded-sm">Aвтомашинууд</div>
-        <div className="bg-sky-600 h-40 w-96 rounded-sm">Үл хөдлөх хөрөнгө</div>
-      </div>
-      <div>
-        <div className="bg-sky-700 h-40 w-96 rounded-sm">Тавилга ба гэрийн чимэглэл</div>
-        <div className="bg-sky-800 h-40 w-96 rounded-sm">Спортын дурсгалт зүйлс</div>
-        <div className="bg-sky-900 h-40 w-96 rounded-sm">Ном ба гар бичмэл</div>
-      </div>
+      <div className='block'>Ангилалууд</div>
+    <div className="container grid-flow-col m-7 justify-center">
+        <Button className="bg-sky-200 h-40 w-96 rounded-sm m-2" value="Электроник">Электроник</Button>
+        <Button className="bg-sky-200 h-40 w-96 rounded-sm m-2" value="Урлаг ба цуглуулга">Урлаг ба цуглуулга</Button>
+        <Button className="bg-sky-300 h-40 w-96 rounded-sm m-2" value="Загвар ба дагалдах хэрэгсэл">Загвар ба дагалдах хэрэгсэл</Button>
+        <Button className="bg-sky-400 h-40 w-96 rounded-sm m-2" value="Үнэт эдлэл ба цаг">Үнэт эдлэл ба цаг</Button>
+        <Button className="bg-sky-500 h-40 w-96 rounded-sm m-2" value="Aвтомашинууд">Aвтомашинууд</Button>
+        <Button className="bg-sky-600 h-40 w-96 rounded-sm m-2" value="Үл хөдлөх хөрөнгө">Үл хөдлөх хөрөнгө</Button>
+        <Button className="bg-sky-700 h-40 w-96 rounded-sm m-2" value="Тавилга ба гэрийн чимэглэл">Тавилга ба гэрийн чимэглэл</Button>
+        <Button className="bg-sky-800 h-40 w-96 rounded-sm m-2" value="Спортын дурсгалт зүйлс">Спортын дурсгалт зүйлс</Button>
+        <Button className="bg-sky-900 h-40 w-96 rounded-sm m-2" value="Ном ба гар бичмэл">Ном ба гар бичмэл</Button>
     </div>
     </div>
-  
   );
 }
