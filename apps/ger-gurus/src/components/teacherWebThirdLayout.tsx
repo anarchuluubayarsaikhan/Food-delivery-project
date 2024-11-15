063d273fe60467d72a1170cb4c51706ed967f213
Miigaa <mytawochir@gmail.com>
2024-11-15 16:49:02 +0800

    585 ger guruslast (#599)

--- apps/ger-gurus/src/components/teacherWebThirdLayout.tsx
+++ apps/ger-gurus/src/components/teacherWebThirdLayout.tsx
@@ -14,28 +14,20 @@ const TeacherWebThirdLayout: React.FC = () => {
   });
 
   const freeVideos = [
-    { id: 1, src: '/home-video.mp4', alt: 'Video 1' },
-    { id: 2, src: '/home-video.mp4', alt: 'Video 2' },
-    { id: 3, src: '/home-video.mp4', alt: 'Video 3' },
+    { id: 1, src: '/vid1.mp4', alt: 'Video 1' },
+    { id: 2, src: '/vid2.mp4', alt: 'Video 2' },
+    { id: 3, src: '/vid3.mp4', alt: 'Video 3' },
   ];
 
   const newVideos = [
-    { id: 1, src: '/home-video.mp4', alt: 'Video 1' },
-    { id: 2, src: '/home-video.mp4', alt: 'Video 2' },
-    { id: 3, src: '/home-video.mp4', alt: 'Video 3' },
+    { id: 1, src: '/vid4.mp4', alt: 'Video 1' },
+    { id: 2, src: '/vid5.mp4', alt: 'Video 2' },
+    { id: 3, src: '/vid6.mp4', alt: 'Video 3' },
   ];
 
-  const freeTexts = [
-    { title: 'Video 1' },
-    { title: 'Video 2' },
-    { title: 'Video 3' },
-  ];
+  const freeTexts = [{ title: 'Video 1' }, { title: 'Video 2' }, { title: 'Video 3' }];
 
-  const newTexts = [
-    { title: 'Video 1' },
-    { title: 'Video 2' },
-    { title: 'Video 3' },
-  ];
+  const newTexts = [{ title: 'Video 1' }, { title: 'Video 2' }, { title: 'Video 3' }];
 
   const handlePlayPause = (section: string, index: number) => {
     const videoId = `${section}-${index}`;
@@ -48,7 +40,7 @@ const TeacherWebThirdLayout: React.FC = () => {
         videoElement.play();
       }
 
-      setIsPlaying(prevState => ({
+      setIsPlaying((prevState) => ({
         ...prevState,
         [videoId]: !prevState[videoId],
       }));
@@ -66,13 +58,7 @@ const TeacherWebThirdLayout: React.FC = () => {
         <div className="flex gap-8 justify-center">
           {freeVideos.map((video, index) => (
             <div key={video.id} className="relative">
-              <video
-                id={`free-${index}`}
-                className="rounded-md shadow-sm w-[450px]"
-                muted
-                loop
-                aria-label={video.alt}
-              >
+              <video id={`free-${index}`} className="rounded-md shadow-sm w-[450px]" muted loop aria-label={video.alt}>
                 <source src={video.src} type="video/mp4" />
               </video>
 
@@ -82,11 +68,7 @@ const TeacherWebThirdLayout: React.FC = () => {
                 onClick={() => handlePlayPause('free', index)}
                 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-800 p-3 rounded-full shadow-lg cursor-pointer"
               >
-                {isPlaying[`free-${index}`] ? (
-                  <Pause className="text-green-600 w-6 h-6" />
-                ) : (
-                  <PlayIcon className="text-green-600 w-6 h-6" />
-                )}
+                {isPlaying[`free-${index}`] ? <Pause className="text-green-600 w-6 h-6" /> : <PlayIcon className="text-green-600 w-6 h-6" />}
               </div>
             </div>
           ))}
@@ -102,13 +84,7 @@ const TeacherWebThirdLayout: React.FC = () => {
         <div className="flex gap-6 justify-center">
           {newVideos.map((video, index) => (
             <div key={video.id} className="relative">
-              <video
-                id={`new-${index}`}
-                className="rounded-md shadow-sm w-[450px]"
-                muted
-                loop
-                aria-label={video.alt}
-              >
+              <video id={`new-${index}`} className="rounded-md shadow-sm w-[450px]" muted loop aria-label={video.alt}>
                 <source src={video.src} type="video/mp4" />
               </video>
 
@@ -118,17 +94,13 @@ const TeacherWebThirdLayout: React.FC = () => {
                 onClick={() => handlePlayPause('new', index)}
                 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-800 p-3 rounded-full shadow-lg cursor-pointer"
               >
-                {isPlaying[`new-${index}`] ? (
-                  <Pause className="text-green-600 w-6 h-6" />
-                ) : (
-                  <PlayIcon className="text-green-600 w-6 h-6" />
-                )}
+                {isPlaying[`new-${index}`] ? <Pause className="text-green-600 w-6 h-6" /> : <PlayIcon className="text-green-600 w-6 h-6" />}
               </div>
             </div>
           ))}
         </div>
       </div>
-      <div className="mt-16 border-green-500 border-t"/>
+      <div className="mt-16 border-green-500 border-t" />
     </div>
   );
 };
